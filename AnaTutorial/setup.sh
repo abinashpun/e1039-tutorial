833e045ab49e8ccf71c3b85614494dcfb89b09e2
abinashpun <apun@spinquestgpvm01.fnal.gov>
2020-01-17 10:51:18 -0600

    root include path

--- AnaTutorial/setup.sh
+++ AnaTutorial/setup.sh
@@ -4,7 +4,7 @@ DIR_TOP=$(dirname $(readlink -f $BASH_SOURCE))
 source /e906/app/software/osg/software/e1039/this-e1039.sh
 
 export LD_LIBRARY_PATH=$DIR_TOP/inst/lib:$LD_LIBRARY_PATH
-
+export ROOT_INCLUDE_PATH=$DIR_TOP/include/:$ROOT_INCLUDE_PATH
 function cmake-ana-tutorial {
     cmake -DCMAKE_INSTALL_PREFIX=$DIR_TOP/inst $DIR_TOP/src
     ret=$?
